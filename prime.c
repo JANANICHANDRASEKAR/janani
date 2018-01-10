e640b833dd49933a8a0212746808983dff96c1c6
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-01-10 16:53:25 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main();
+{
+int n,i,count;
+count=0;
+for(i=2;i<=n;i++)
+{
+int(n%i==0)
+{
+count++;
+}
+}
+if(count==1)
+{
+printf("prime");
+}
+else
+{
+printf("not prime")
+}
+}

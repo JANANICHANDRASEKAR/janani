ccb9b21ca79753eddc0ed4e04e59cb729cf138e7
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-01-19 16:52:49 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -3,4 +3,22 @@ void main()
 {
 int a,d,r;
 printf("enter the number");
-scanf("%d",
+scanf("%d",&a);
+  rev=0;
+ d=a;
+ while(a!=0)
+ {
+   r=a%10;
+   rev=rev*10=r;
+   a=a/10;
+ }
+  if(d==rev)
+  {
+    printf("palindrome");
+    }
+    else
+    {
+      printf("not");
+    }
+}
+

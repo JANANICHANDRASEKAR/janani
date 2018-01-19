5ecb4eb14c2ade63f724a530f1a9a1d5aa8bed28
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-01-19 16:42:48 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n,limit,m=0;
+printf("table n0 and limit");
+scanf("%d%d",&n,&limit);
+printf("multiplication table");
+for(inti=1;i<=limit;i++)
+{
+m=i*n;
+printf("%d*%d=%d",i,n,m);
+}
+]

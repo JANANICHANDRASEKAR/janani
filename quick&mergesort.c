4a39a82c4c9be48ed02fc717c68fb0472e5446ba
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-01-21 04:50:08 +0000

    Create quick&mergesort.c

---
+++ quick&mergesort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() 
+{
+	int a[1000],i,j,t,l;
+	scanf("%d",&l);
+	for(i=0;i<l;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<l;i++)
+	{
+		for(j=0;j<l;j++)
+		{
+			if(a[i]>a[j])
+			{
+				t=a[j];
+				a[j]=a[i];
+				a[i]=t;
+			 }
+			
+		}
+	}
+	printf("%d\t",a[l%2]);
+	return 0;
+}

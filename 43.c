1e4a8605c77a2b525698d4a705521494d8c7db7e
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-02-07 16:49:50 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+char b[10];
+printf("enter the value");
+scanf("%s",&a);
+printf("enter the value");
+scanf("%s",&b);
+printf("enter the concatenate sting%s%s",a,b);
+}
